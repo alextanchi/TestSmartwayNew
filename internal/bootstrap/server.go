aeddc024fe9e17fe1ca95652cf92b418e21ccd98
alextanchi <alex.tanz@mail.ru>
2024-03-15 18:51:10 +0500

    update_2.9.3 update service.go

--- internal/bootstrap/server.go
+++ internal/bootstrap/server.go
@@ -16,7 +16,7 @@ type Server struct {
 func NewServer(cnt controller.Controller) Server {
 	return Server{
 		httpServer: &http.Server{
-			Addr:           ":" + port,
+			Addr:           ":8080",
 			MaxHeaderBytes: 1 << 20,          //1MB
 			ReadTimeout:    10 * time.Second, //10 сек
 			WriteTimeout:   10 * time.Second,
@@ -30,19 +30,10 @@ func (s Server) InitRoutes() *gin.Engine {
 	router := gin.New()
 	router.POST("/employee", s.cnt.AddEmployee)
 	router.DELETE("/employee/:id", s.cnt.DeleteEmployee)
-	router.GET("/employee/list/:companyid", s.cnt.ListEmployeeByCompanyId)
-	router.GET("/employee/list/:department", s.cnt.ListEmployeeByDepartment)
+	router.GET("/employee/list/company/:companyid", s.cnt.ListEmployeeByCompanyId)
+	router.GET("/employee/list/department/:department", s.cnt.ListEmployeeByDepartment)
 	router.PUT("/employee", s.cnt.UpdateEmployee)
 
 	return router
 
 }
-
-
-func (s *Server) Run(port string, handler http.Handler) error { 
-
-	return s.httpServer.ListenAndServe() 
-
-func (s *Server) Shutdown(ctx context.Context) error { // остановка сервера
-	return s.httpServer.Shutdown(ctx)
-}*/
